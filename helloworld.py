5766e889032b6fd75c646aa849f7725fe348bcc6
Mythileeswari <eswarimythili4@gmail.com>
2024-09-23 10:57:16 +0530

    Commit Message

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,6 @@
 #string value using pattern in helloworld
-'''j='hello_world'
+j='hello_world'
 for i in range(len(j)):
     for m in range(i+1):
         print(j[m],end='')
-    print("\n")'''
+    print("\n")
